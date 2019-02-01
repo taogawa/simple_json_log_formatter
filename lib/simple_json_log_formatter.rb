a171660e47bc1d83d755dd3ef70a4307a3efe3d5
taogawa <taogwjp@gmail.com>
2019-02-02 01:18:30 +0900

    Polish

--- lib/simple_json_log_formatter.rb
+++ lib/simple_json_log_formatter.rb
@@ -18,25 +18,29 @@ class SimpleJsonLogFormatter
     @opts[:severity_key] = :severity unless @opts.has_key?(:severity_key)
     @opts[:progname_key] = :progname unless @opts.has_key?(:progname_key)
     @opts[:message_key] ||= :message
-    @opts[:datetime_format] = @opts.dig(:datetime_format)
+    @opts[:datetime_format] = "%FT%T%:z" unless @opts.has_key?(:datetime_format)
   end
 
   def call(severity, time, progname, msg)
-    log = [*format_time(time), *format_severity(severity), *format_progname(progname), *format_message(msg)].to_h
+    log = {}
+    log.merge!(format_time(time)) if @opts[:time_key]
+    log.merge!(format_severity(severity)) if @opts[:severity_key]
+    log.merge!(format_progname(progname)) if @opts[:progname_key]
+    log.merge!(format_message(msg))
     "#{log.to_json}\n"
   end
 
   private
     def format_time(time)
-      { @opts[:time_key] => time.strftime(@opts.dig(:datetime_format) || "%FT%T%:z") } if @opts[:time_key]
+      { @opts[:time_key] => time.strftime(@opts[:datetime_format]) }
     end
 
     def format_severity(severity)
-      { @opts[:severity_key] => severity } if @opts[:severity_key]
+      { @opts[:severity_key] => severity }
     end
 
     def format_progname(progname)
-      { @opts[:progname_key] => progname } if @opts[:progname_key]
+      { @opts[:progname_key] => progname }
     end
 
     def format_message(msg)
